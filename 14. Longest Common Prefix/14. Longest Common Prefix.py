e088d069b2e4b421bfe7067368b69f51a6fff7b3
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:26 +0530

    Add solution: 14. Longest Common Prefix

---
+++ 14. Longest Common Prefix/14. Longest Common Prefix.py
@@ -0,0 +1,15 @@
+#
+# Problem: 14. Longest Common Prefix
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/longest-common-prefix/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
