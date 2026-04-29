a3edd79c292c3f827f7f3acd78eae1c8c38890ab
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:38 +0530

    Add solution: 383. Ransom Note

---
+++ 383. Ransom Note/383. Ransom Note.py
@@ -0,0 +1,16 @@
+#
+# Problem: 383. Ransom Note
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/ransom-note/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        
