eda90d9be73bc176f1d34fa2a7a18530552e2398
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:44 +0530

    Add solution: 169. Majority Element

---
+++ 169. Majority Element/169. Majority Element.py
@@ -0,0 +1,15 @@
+#
+# Problem: 169. Majority Element
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/majority-element/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
