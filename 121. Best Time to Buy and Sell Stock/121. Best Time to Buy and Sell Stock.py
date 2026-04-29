00ba30bfbf03de0ee43ba64cbbdc9106e4480be4
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:40 +0530

    Add solution: 121. Best Time to Buy and Sell Stock

---
+++ 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,15 @@
+#
+# Problem: 121. Best Time to Buy and Sell Stock
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
