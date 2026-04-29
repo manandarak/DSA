99dfa10562dc65223332222e45f13065218e501c
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:02:30 +0530

    Add solution: 424. Longest Repeating Character Replacement

---
+++ 424. Longest Repeating Character Replacement/424. Longest Repeating Character Replacement.py
@@ -0,0 +1,16 @@
+#
+# Problem: 424. Longest Repeating Character Replacement
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/longest-repeating-character-replacement/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        
