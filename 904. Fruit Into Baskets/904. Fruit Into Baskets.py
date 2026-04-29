c8d19b3e7c8f036042b8f168e29cb612bd59cf75
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:02:40 +0530

    Add solution: 904. Fruit Into Baskets

---
+++ 904. Fruit Into Baskets/904. Fruit Into Baskets.py
@@ -0,0 +1,15 @@
+#
+# Problem: 904. Fruit Into Baskets
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/fruit-into-baskets/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def totalFruit(self, fruits):
+        """
+        :type fruits: List[int]
+        :rtype: int
+        """
+        
