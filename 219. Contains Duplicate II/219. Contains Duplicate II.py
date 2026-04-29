2b313561d17d37e9d2aac0a760b819f06173ba9e
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:39 +0530

    Add solution: 219. Contains Duplicate II

---
+++ 219. Contains Duplicate II/219. Contains Duplicate II.py
@@ -0,0 +1,16 @@
+#
+# Problem: 219. Contains Duplicate II
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/contains-duplicate-ii/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        
