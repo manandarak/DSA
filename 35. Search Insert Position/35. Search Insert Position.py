a976f841e9e245e7f65e1913cacdc50759d66e3b
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:02:41 +0530

    Add solution: 35. Search Insert Position

---
+++ 35. Search Insert Position/35. Search Insert Position.py
@@ -0,0 +1,16 @@
+#
+# Problem: 35. Search Insert Position
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/search-insert-position/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
