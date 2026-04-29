d2fa90df0a8d142b4b8c43948e5f2528aa5b2ff2
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:04:21 +0530

    Add solution: 3Sum

---
+++ 3Sum/3Sum.py
@@ -0,0 +1,15 @@
+#
+# Problem: 3Sum
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/3sum/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
