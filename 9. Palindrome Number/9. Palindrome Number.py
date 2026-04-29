f1fbb78f9bd599a510079a70c7f8170b95915e33
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:51 +0530

    Add solution: 9. Palindrome Number

---
+++ 9. Palindrome Number/9. Palindrome Number.py
@@ -0,0 +1,15 @@
+#
+# Problem: 9. Palindrome Number
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/palindrome-number/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
