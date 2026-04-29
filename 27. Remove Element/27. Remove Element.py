d550f56c67aa4b60393a708a40016e27a9f67f15
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:42 +0530

    Add solution: 27. Remove Element

---
+++ 27. Remove Element/27. Remove Element.py
@@ -0,0 +1,16 @@
+#
+# Problem: 27. Remove Element
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/remove-element/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
