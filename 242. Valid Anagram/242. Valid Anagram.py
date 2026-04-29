df37f1f8f0957516bfa23170fe1168da967a4686
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:27 +0530

    Add solution: 242. Valid Anagram

---
+++ 242. Valid Anagram/242. Valid Anagram.py
@@ -0,0 +1,16 @@
+#
+# Problem: 242. Valid Anagram
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/valid-anagram/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
