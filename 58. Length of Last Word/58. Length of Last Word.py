749920273f117f8f05b9c806ac33377797b6ccad
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:46 +0530

    Add solution: 58. Length of Last Word

---
+++ 58. Length of Last Word/58. Length of Last Word.py
@@ -0,0 +1,15 @@
+#
+# Problem: 58. Length of Last Word
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/length-of-last-word/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
