14b5431769673d5826964200c8df6e26c4668460
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:36 +0530

    Add solution: 205. Isomorphic Strings

---
+++ 205. Isomorphic Strings/205. Isomorphic Strings.py
@@ -0,0 +1,16 @@
+#
+# Problem: 205. Isomorphic Strings
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/isomorphic-strings/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
