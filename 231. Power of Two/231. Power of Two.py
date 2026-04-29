459e84850d84cd0944b85ad2f87227f63c5d9c07
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:02:42 +0530

    Add solution: 231. Power of Two

---
+++ 231. Power of Two/231. Power of Two.py
@@ -0,0 +1,15 @@
+#
+# Problem: 231. Power of Two
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/power-of-two/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        
