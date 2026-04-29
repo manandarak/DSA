72d2e43bd0bc694f8f98c6b9efa5a3f67050ee2d
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:47 +0530

    Add solution: 136. Single Number

---
+++ 136. Single Number/136. Single Number.py
@@ -0,0 +1,15 @@
+#
+# Problem: 136. Single Number
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/single-number/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
