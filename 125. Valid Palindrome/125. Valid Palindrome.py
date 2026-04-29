c7d4cd2681831f94bf023ee4388b227cc3cdbf26
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:49 +0530

    Add solution: 125. Valid Palindrome

---
+++ 125. Valid Palindrome/125. Valid Palindrome.py
@@ -0,0 +1,15 @@
+#
+# Problem: 125. Valid Palindrome
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/valid-palindrome/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
