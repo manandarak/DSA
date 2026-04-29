7c341101f553c898d36aa79a1d0785ce4078d669
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:02:45 +0530

    Add solution: 209. Minimum Size Subarray Sum

---
+++ 209. Minimum Size Subarray Sum/209. Minimum Size Subarray Sum.py
@@ -0,0 +1,10 @@
+#
+# Problem: 209. Minimum Size Subarray Sum
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/minimum-size-subarray-sum/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
