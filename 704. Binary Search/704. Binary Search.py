fe324534731d92510bc0201674f067bf8c694e94
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:03:34 +0530

    Add solution: 704. Binary Search

---
+++ 704. Binary Search/704. Binary Search.py
@@ -0,0 +1,16 @@
+#
+# Problem: 704. Binary Search
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/binary-search/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
