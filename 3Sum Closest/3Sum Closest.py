472a44e02821226fbc8fd6cdd1aabd8580ed62b7
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:06:19 +0530

    Add solution: 3Sum Closest

---
+++ 3Sum Closest/3Sum Closest.py
@@ -0,0 +1,16 @@
+#
+# Problem: 3Sum Closest
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/3sum-closest/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
