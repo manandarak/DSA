a962d092d63eb3a83dcb8b616039024e47f7d50d
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:29 +0530

    Add solution: 66. Plus One

---
+++ 66. Plus One/66. Plus One.py
@@ -0,0 +1,15 @@
+#
+# Problem: 66. Plus One
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/plus-one/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        
