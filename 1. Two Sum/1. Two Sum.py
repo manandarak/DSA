010164ae145d1f5620ac28aa856dcdf01ed298aa
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:50 +0530

    Add solution: 1. Two Sum

---
+++ 1. Two Sum/1. Two Sum.py
@@ -0,0 +1,16 @@
+#
+# Problem: 1. Two Sum
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/two-sum/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
