8dd08aff6b7383491460001ac2601fd65113bce1
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:02:43 +0530

    Add solution: 75. Sort Colors

---
+++ 75. Sort Colors/75. Sort Colors.py
@@ -0,0 +1,15 @@
+#
+# Problem: 75. Sort Colors
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/sort-colors/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
