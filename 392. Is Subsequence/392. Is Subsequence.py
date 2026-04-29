69ab678bd0e038a9f19e43e4888a0c37f54586e3
Manan Darak <134354508+manandarak@users.noreply.github.com>
2026-04-29 14:05:33 +0530

    Add solution: 392. Is Subsequence

---
+++ 392. Is Subsequence/392. Is Subsequence.py
@@ -0,0 +1,16 @@
+#
+# Problem: 392. Is Subsequence
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/is-subsequence/description/
+# Language: python
+# Date: 2026-04-29
+
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
